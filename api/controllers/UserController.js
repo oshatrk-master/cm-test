5c4a6dd3dfbb3853e0010883f9b52255be6df6d3
oshatrk <user@example.com>
2017-04-08 14:33:03 +0300

    Явное или неявное подключение действий в контроллере? Пока оставлено неявное (автоматическое).

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -3,8 +3,13 @@
  *
  * @description :: Server-side logic for managing users
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ *
  */
 
+// Подключение действий к контроллеру.
+
+// Если выполнить подключение в таком-же неявном стиле, как это сделано в Sails,
+// то автодополнение не будет работать в Ide:
 
 const requireAndBindActions = require('../requireAndBindActions')
 
@@ -12,3 +17,22 @@ const UserController = requireAndBindActions(__filename);
 
 module.exports = UserController;
 
+// // Явное подключение будет работать в Ide, но требует ручного обновления
+// // для добавления, удаления и переименования действий:
+// /**
+//  * @class sails.controllers.user
+//  */
+// module.exports = {
+//   after_register: require('./UserController/after_register'),
+//   avatar: require('./UserController/avatar'),
+//   confirm: require('./UserController/confirm'),
+//   friends: require('./UserController/friends'),
+//   list: require('./UserController/list'),
+//   login: require('./UserController/login'),
+//   logout: require('./UserController/logout'),
+//   profile: require('./UserController/profile'),
+//   register: require('./UserController/register'),
+//   request: require('./UserController/request'),
+//   requests: require('./UserController/requests'),
+//   subscribe: require('./UserController/subscribe')
+// };
